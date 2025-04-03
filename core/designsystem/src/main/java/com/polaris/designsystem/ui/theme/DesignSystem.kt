8f03809d0e96c4d46181ddb75afb7535cb2a2c90
polaris <61411085+polaris428@users.noreply.github.com>
2025-04-03 22:19:27 +0900

    ✨초기 init 데이터 변경

--- core/designsystem/src/main/java/com/polaris/designsystem/ui/theme/DesignSystem.kt
+++ core/designsystem/src/main/java/com/polaris/designsystem/ui/theme/DesignSystem.kt
@@ -371,37 +371,22 @@ fun CDSColumn(
 
 var dummyDateList = listOf(
     ClipboardItem(
-        type = "구글",
-        url = "https://www.google.com/search?q=%EB%84%A4%EC%9D%B4%EB%B2%84&oq=&gs_lcrp=...",
-        title = "Google",
-        faviconUrl = "https://www.google.com/s2/favicons?sz=64&domain_url=https://www.google.com/search?q=%EB%84%A4%EC%9D%B4%EB%B2%84&oq=&gs_lcrp=...",
-        timestamp = 1740930924019,
-        isPinned = false
+        type = "사용 설명서",
+        url = "https://www.notion.so/Clipy-1cad32a959ab80ce9508d97548f9292f?pvs=4",
+        title = "클리피에 오신걸 환영합니다",
+        faviconUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzj_kb1-Nj0Vv1nWFlVXvnFW_-ayB0AGYpcg&s",
+        timestamp = System.currentTimeMillis(),
+        isPinned = true
     ),
     ClipboardItem(
-        type = "네이버 포스트",
-        url = "https://m.post.naver.com/viewer/postView.naver?volumeNo=38357237&memberNo=57847637",
-        title = "[24년도] 콘서트 망원경 이 글 하나로 종결 : 네이버 포스트",
-        faviconUrl = "https://www.google.com/s2/favicons?sz=64&domain_url=https://m.post.naver.com/viewer/postView.naver?volumeNo=38357237&memberNo=57847637",
-        timestamp = 1741153750278,
+        type = "벨로그",
+        url = "https://velog.io/",
+        title = "벨로그",
+        faviconUrl = "https://assets.disquiet.io/images/product/thumbnail/71e4d050cc5588bf30aea2387817b22f8ed74fe1564678be862e341fb6ce5fe9",
+        timestamp = System.currentTimeMillis(),
         isPinned = false
     ),
-    ClipboardItem(
-        type = "ㅇㅅㅇ",
-        url = "https://m.search.naver.com/search.naver?query=88%ED%8F%AC%EC%B0%A8+%EB%A9%94%EB%89%B4",
-        title = "88포차 메뉴 : 네이버 검색",
-        faviconUrl = "https://www.google.com/s2/favicons?sz=64&domain_url=https://m.search.naver.com/search.naver?query=88%ED%8F%AC%EC%B0%A8+%EB%A9%94%EB%89%B4",
-        timestamp = 1741157005289,
-        isPinned = false
-    ),
-    ClipboardItem(
-        type = "Google",
-        url = "https://www.google.com/search?client=ms-android-samsung-ss&q=lindor+%EC%B4%88%EC%BD%9C%EB%A6%BF+%EC%95%8C%EC%BD%9C",
-        title = "lindor 초콜릿 알콜 - Google 검색",
-        faviconUrl = "https://www.google.com/s2/favicons?sz=64&domain_url=https://www.google.com/search?client=ms-android-samsung-ss&q=lindor+%EC%B4%88%EC%BD%9C%EB%A6%BF+%EC%95%8C%EC%BD%9C",
-        timestamp = 1741264638943,
-        isPinned = false
-    )
+
 )
 val dummyData = ClipboardItem(
     type = "GitHub",
